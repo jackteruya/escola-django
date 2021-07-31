cd1df509c7a846f00e5663685d6a6cf5c9e0d157
jackteruya <jack.teruya@gmail.com>
2021-07-30 23:15:45 -0400

    Criação da API v2 com ViewSet

--- cursos/views.py
+++ cursos/views.py
@@ -1,8 +1,16 @@
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
 
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
+
+"""
+API V1
+"""
 
 
 class CursosAPIView(generics.ListCreateAPIView):
@@ -35,3 +43,24 @@ class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
                                      curso_id=self.kwargs.get('curso_pk'),
                                      pk=self.kwargs.get('avaliacao_pk'))
         return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+
+
+"""
+API v2
+"""
+
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
